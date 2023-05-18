2b9360bb4dd0d02c3c4e9be2bf9571afbc70ba1c
vfcash <50428242+vfcash@users.noreply.github.com>
2023-05-18 10:22:51 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -7,12 +7,12 @@ apt install --assume-yes mingw-w64
 apt install --assume-yes gcc
 apt install --assume-yes clang
 
-gcc -Ofast -fopenmp miner.c ecc.c base58.c -lm -o bin/gcc-miner-linux-64
-clang -Ofast -fopenmp miner.c ecc.c base58.c -lm -o bin/clang-miner-linux-64
+gcc -Ofast -mrdseed -fopenmp miner.c ecc.c base58.c -lm -o bin/gcc-miner-linux-64
+clang -Ofast -mrdseed -fopenmp miner.c ecc.c base58.c -lm -o bin/clang-miner-linux-64
 i686-w64-mingw32-gcc -Ofast -fopenmp miner.c ecc.c base58.c -static -static-libgcc -lm -o bin/miner-win-64.exe
 
-gcc -Ofast -fopenmp gminer.c sha3.c ecc.c base58.c -lSDL2 -lm -o bin/gcc-gui-miner-linux-64
-clang -Ofast -fopenmp gminer.c sha3.c ecc.c base58.c -lSDL2 -lm -o bin/clang-gui-miner-linux-64
+gcc -Ofast -mrdseed -fopenmp gminer.c sha3.c ecc.c base58.c -lSDL2 -lm -o bin/gcc-gui-miner-linux-64
+clang -Ofast -mrdseed -fopenmp gminer.c sha3.c ecc.c base58.c -lSDL2 -lm -o bin/clang-gui-miner-linux-64
 i686-w64-mingw32-gcc -Ofast -fopenmp gminer.c sha3.c ecc.c base58.c -static -static-libgcc -lSDL2 -lm -o bin/gui-miner-win-64.exe
 
 
